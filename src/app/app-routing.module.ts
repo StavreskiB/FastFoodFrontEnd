b09a9af95b2ca90971da9b8d52e5866381e41b0d
StavreskiB <stavreskibojan@gmail.com>
2021-11-23 01:43:10 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {path : '', component : HeaderComponent},
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { 
+  
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { LoginService } from './services/login.service';
+import { ManagementComponent } from './components/management/management.component';
+import { ManagementService } from './services/management.service';
+import { ServiceService } from './services/service.service';
+import { ServiceComponent } from './components/service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    HeaderComponent,
+    LoginComponent,
+    LoginService,
+    ManagementComponent,
+    ManagementService,
+    ServiceService,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
